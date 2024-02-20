e75ca7721e46f2c861eaa4db37c122d7c1b5396b
Byzon12 <byzoneochieng@gmail.com>
2024-02-20 23:50:03 +0300

    for now

--- frontend.js
+++ frontend.js
@@ -2,6 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const Navbar = document.querySelector('.Navbar');
     const Bar = document.querySelector('.bar');
     const Close =document.querySelector('.close')
+    var MainImage= document.getElementById("mainImg")
+    var smallImage = document.getElementsByClassName("smallImg")
+
+    smallImage[0].addEventListener('click', ()=>{
+        MainImage.src =smallImage[0].src
+       
+    })
+    smallImage[1].addEventListener('click', ()=>{
+        MainImage.src =smallImage[1].src
+       
+    })
+    smallImage[2].addEventListener('click', ()=>{
+        MainImage.src =smallImage[2].src
+       
+    })
+    smallImage[3].addEventListener('click', ()=>{
+        MainImage.src =smallImage[3].src
+       
+    })
+   
+   
 if(Bar)
 {
     Bar.addEventListener('click', () => {
